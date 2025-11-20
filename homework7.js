f98d3ec0dffa78e9ee71e87cfeaebf7dc6329eb2
Kateryna Masiakina <kettmas@gmail.com>
2025-11-20 22:28:12 +0100

    Add new homework

---
+++ homework7.js
@@ -0,0 +1,18 @@
+var str = "Wonderful, Joyful, Happiness, Time, Task, Apple"; //можна було одразу записвти іх у масиві, тоді не потрібен би був спліт
+var words = str.split(/[\s,]+/);
+var re = /^[^aA]{6,}$/;
+for (var i = 0; i < words.length; i++) {
+   if (re.test(words[i])) {
+   console.log(words[i]);
+  }
+}
+
+
+/*в цілому регулярка така і якшо вивести в консоль слова - на варінти які не підходять під умови стоїть false:
+var re = /^[^aA]{6,}$/; 
+console.log(re.test("Wonderful"));
+console.log(re.test("Happiness"));
+console.log(re.test("Joyful"));
+console.log(re.test("Time"));
+console.log(re.test("Task"));
+console.log(re.test("Apple"));*/
